86b78a2f0a78cc750c20bf804ae5c717942f9c11
hidran@gmail.com <hidran@gmail.com>
2020-10-22 01:07:16 +0200

    041. Animations and make our bunny run

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,32 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rigidBody;
     public float thrust = 10.0f;
     public LayerMask groundLayerMask;
+    public Animator animator;
+    public float runSpeed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator.SetBool("isAlive", true);
     }
-
+    private void FixedUpdate()
+    {
+        if(rigidBody.velocity.x < runSpeed)
+        {
+            rigidBody.velocity = new Vector2(runSpeed, rigidBody.velocity.y);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)
+
+        bool isOnTheGround = IsOnTheGround();
+        animator.SetBool("isGrounded", isOnTheGround);
+        if ((Input.GetMouseButtonDown(0)
 
             || Input.GetKeyDown(KeyCode.Space)
             || Input.GetKeyDown(KeyCode.W)
+            ) && isOnTheGround
             )
         {
             Jump();
@@ -28,15 +41,14 @@ public class PlayerController : MonoBehaviour
     }
     void Jump()
     {
-        if (IsOnTheGround())
-        {
+       
             rigidBody.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
-        }
+        
         
     }
     bool IsOnTheGround()
     {
-        print("mask=" + groundLayerMask);
+       
      return Physics2D.Raycast(this.transform.position, Vector2.down,
             1.0f, groundLayerMask.value) ;
        
