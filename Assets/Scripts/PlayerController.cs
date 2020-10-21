58754ad1d902e9aa490980dceed1a0da70d1fd35
hidran@gmail.com <hidran@gmail.com>
2020-10-21 18:24:38 +0200

    040. RigidBody.AddForce. Let's make our bunny jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public float thrust = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)
+
+            || Input.GetKeyDown(KeyCode.Space)
+            || Input.GetKeyDown(KeyCode.W)
+            )
+        {
+            Jump();
+        }
+
+    }
+    void Jump()
+    {
+        rigidBody.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
+    }
+}
