4f398ca0ebb50c28f5a9032707c2e8e8d94a70de
hidran@gmail.com <hidran@gmail.com>
2020-10-27 01:37:01 +0100

    051.Extending levels. Add and remove blocks with block leave trigger

---
+++ Assets/Scripts/LeaveTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaveTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            LevelGenerator.sharedInstance.AddNewBlock();
+
+            LevelGenerator.sharedInstance.RemoveOldBlock();
+        }
+    }
+}
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -64,4 +64,10 @@ public class LevelGenerator : MonoBehaviour
         block.transform.position = blockPosition;
         currentBlocks.Add(block);
     }
+     public void RemoveOldBlock()
+    {
+        var oldblock = currentBlocks[0];
+        currentBlocks.Remove(oldblock);
+        Destroy(oldblock.gameObject);
+    }
 }
